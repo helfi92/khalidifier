a61fbea7b98f2e30f733a45a146bcd0b3a236e3d
Monty Thibault <montythibault@gmail.com>
2016-02-20 14:33:44 -0500

    Merged replace.js with myscript.js

--- myscript.js
+++ myscript.js
@@ -7,3 +7,18 @@ if (document.title.indexOf("Google") != -1) {
     //Appending to DOM 
     document.body.appendChild(btn);
 }
+
+
+quotes = [
+	"Smh they get mad when u have joy.",
+	"Baby, you smart. I want you to film me taking a shower."
+];
+
+Array.prototype.choose = function() {
+	return this[Math.floor(Math.random() * this.length];
+};
+
+// Replaces all quotes
+$('*:not(:has(*))').text(function() {
+	return $(this).text().replace(/"+"/g, '"' + quotes.choose() + '"');
+}); 
